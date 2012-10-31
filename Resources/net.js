88d812254473c959beece735f283db1272156d31
kmmx <kmmx@MacBook-Pro-de-kmmx-2.local>
2012-10-31 12:54:57 -0600

    Modulo network terminado aunque no probado

--- Resources/net.js
+++ Resources/net.js
@@ -5,8 +5,43 @@
 (function(){
 	th.net = {};
 	
+	var serverURL = 'http://bountyhunterapp.appspot.com/bounties';
 	
 	
+	// Funcion para determinar los fugitivos desde un servidor remoto
+	th.net.getFugitives = function(_cb){
+		
+		var xhr = Ti.Network.createHTTPClient();
+		xhr.onload = function(){
+			
+			// La propiedad response text, guarda lo recibido
+			// Sòlo es necesario convertirlo en objeto con JSON.parse
+			_cb(JSON.parse(this.responseText));
+		}
+		
+		xhr.open('GET', serverURL);
+		xhr.send();
+	}
+	
+	// Función pora cambiar el estado de un fugitivo
+	th.net.bustFugitive = function(_udid, _cb){
+		
+		Ti.App.info('Busting Fugitive ' + _udid);
+		
+		var xhr = Ti.Network.createHTTPClient();
+		xhr.onload = function(){
+			_cb(JSON.parse(this.responseText));
+		}
+		
+		xhr.open('POST', serverURL);
+		
+		// Al ser POST, lo que hacemos es agregar
+		// los datos en el mètodo send, en el caso
+		// de get, no es necesario
+		xhr.send({
+			udid : _udid
+		});
+	}
 	
 	
 	
